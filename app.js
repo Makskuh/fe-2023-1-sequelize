abc5d66d373b7787641dde0053592aadbcc0e7b3
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-17 18:39:07 +0300

    Add unfinished UserController with middlewares

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const UserController = require('./controllers/userController');
 
 const app = express();
 
+const bodyParser = express.json();
+
 app.get('/', (req, res) => {
-  res.send('test')
-})
+  res.send('test');
+});
+
+app.post(
+  '/users',
+  bodyParser,
+  (req, res, next) => {
+    req.test = 213545;
+    next();
+  },
+  UserController.createUser
+);
 
 module.exports = app;
