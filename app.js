46ddf840806784bd13aa02045f0fcfe044b2dc30
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-17 18:51:18 +0300

    Add route example

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ const app = express();
 
 const bodyParser = express.json();
 
-app.get('/', (req, res) => {
-  res.send('test');
-});
+// app.get('/users', UserController.getUsers);
 
-app.post(
-  '/users',
-  bodyParser,
-  (req, res, next) => {
-    req.test = 213545;
-    next();
-  },
-  UserController.createUser
-);
+// app.post(
+//   '/users',
+//   bodyParser,
+//   (req, res, next) => {
+//     req.test = 213545;
+//     next();
+//   },
+//   UserController.createUser
+// );
+
+app
+  .route('/users')
+  .get(UserController.getUsers)
+  .post(bodyParser, UserController.createUser);
+
+app.delete('/users/:userId', UserController.deleteUser);
 
 module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,4 +8,10 @@ module.exports.getUser = (req, res) => {};
 
 module.exports.updateUser = (req, res) => {};
 
-module.exports.deleteUser = (req, res) => {};
+module.exports.deleteUser = (req, res) => {
+  const {
+    params: { userId },
+  } = req;
+
+  res.send(`user ${userId} is deleted`);
+};
