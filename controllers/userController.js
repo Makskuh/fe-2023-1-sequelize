14c186760f4e40fd9017f9a2bd0dab2bd4d61610
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-18 20:00:27 +0300

    Add GET multiple users controller handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { User } = require('../models');
 
 module.exports.createUser = async (req, res) => {
@@ -10,7 +11,37 @@ module.exports.createUser = async (req, res) => {
   res.send({ data: user });
 };
 
-module.exports.getUsers = (req, res) => {};
+module.exports.getUsers = async (req, res) => {
+  // const users = await User.findAll(); // SELECT * from users;
+
+  // const users = await User.findAll({
+  //   attributes: ['firstName', 'email', 'isMale']
+  // }); // SELECT firstName, email, isMale from users;
+
+  // const users = await User.findAll({
+  //   attributes: [['first_name', 'name'], 'email', 'isMale'],
+  // }); // SELECT firstName as name, email, isMale from users;
+
+  // const users = await User.findAll({
+  //   attributes: {
+  //     exclude: ['password', 'createdAt', 'updatedAt'],
+  //   },
+  // }); // SELECT все крім password, createdAt, updatedAt from users;
+
+  // const users = await User.findAll({
+  //   where: {
+  //     isMale: true,
+  //   },
+  // }); // SELECT * from users WHERE isMale = true;
+
+  const users = await User.findAll({
+    where: {
+      [Op.or]: [{ firstName: 'User' }, { id: 2 }],
+    },
+  }); // SELECT * from users WHERE isMale = true;
+
+  res.send({ data: users });
+};
 
 module.exports.getUser = (req, res) => {};
 
