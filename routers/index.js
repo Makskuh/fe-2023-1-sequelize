9c65b61ce7af21e10aaa3d9a4fd1bbbf23bfeb29
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-20 19:02:39 +0300

    Add separate routers

--- routers/index.js
+++ routers/index.js
@@ -1,51 +1,9 @@
 const express = require('express');
-const UserController = require('../controllers/userController');
-const TaskController = require('../controllers/taskController');
-const { checkUserExistanceMW } = require('../middlewares/users.mv');
-const userRouter = express.Router();
+const rootRouter = express.Router();
+const userRouter = require('./userRouter');
+const taskRouter = require('./taskRouter');
 
-// userRouter.use
-// userRouter.post('/asdsfas')
+rootRouter.use(userRouter);
+rootRouter.use(taskRouter);
 
-userRouter
-  .route('/users')
-  .get(UserController.getUsers)
-  .post(UserController.createUser);
-
-userRouter
-  .route('/users/:userId')
-  .get(UserController.getUser)
-  .put(UserController.updateUser)
-  .delete(UserController.deleteUser);
-
-userRouter.post(
-  '/users/:userId/tasks',
-  checkUserExistanceMW,
-  TaskController.createTask
-);
-userRouter.get(
-  '/users/:userId/tasks',
-  checkUserExistanceMW,
-  TaskController.getTasks
-);
-
-userRouter.get(
-  '/users/:userId/tasks/:taskId',
-  checkUserExistanceMW,
-  TaskController.getTask
-);
-userRouter.put(
-  '/users/:userId/tasks/:taskId',
-  checkUserExistanceMW,
-  TaskController.updateTask
-);
-userRouter.delete(
-  '/users/:userId/tasks/:taskId',
-  checkUserExistanceMW,
-  TaskController.deleteTask
-);
-
-userRouter.put('/users/v2/:userId', UserController.updateUserInstance);
-userRouter.delete('/users/v2/:userId', UserController.deleteUserInstance);
-
-module.exports = userRouter;
+module.exports = rootRouter;
