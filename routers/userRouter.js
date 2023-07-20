b5c1105d07eb90d930488c61b144f3fa23cb0235
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-20 20:24:04 +0300

    Add unfinished group controller with routing

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const taskRouter = require('./taskRouter');
 const UserController = require('../controllers/userController');
+const GroupController = require('../controllers/groupController');
 const { checkUserExistanceMW } = require('../middlewares/users.mv');
 const userRouter = express.Router();
 
@@ -17,6 +18,8 @@ userRouter
 
 userRouter.put('/v2/:userId', UserController.updateUserInstance);
 userRouter.delete('/v2/:userId', UserController.deleteUserInstance);
+
+userRouter.get('/:userId/groups', GroupController.getUserGroups)
 
 userRouter.use('/:userId/tasks', checkUserExistanceMW, taskRouter);
 
