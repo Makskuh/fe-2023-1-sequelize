b909661cbf56aed83e36b9305f2f14b95cbe2926
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-21 20:32:21 +0300

    Add image upload example

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -30,17 +30,18 @@ module.exports.getUserGroups = async (req, res, next) => {
     } = req;
 
     const userWithGroups = await User.findByPk(userId, {
-      include: [{
-        model: Group,
-        through: {
-          attributes: [],
+      include: [
+        {
+          model: Group,
+          through: {
+            attributes: [],
+          },
+          attributes: ['id', 'name', 'description', 'imagePath'],
         },
-        attributes: ['id', 'name', 'description', 'imagePath'],
-      },
-      // {
-      //   model: Task
-      // }
-    ],
+        // {
+        //   model: Task
+        // }
+      ],
     });
 
     if (!userWithGroups) {
@@ -50,6 +51,44 @@ module.exports.getUserGroups = async (req, res, next) => {
     // const groups = await user.getGroups();
 
     res.send({ data: userWithGroups });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.addImageToGroup = async (req, res, next) => {
+  try {
+    const {
+      params: { groupId },
+      file: { filename },
+    } = req;
+
+    /*
+    {
+        "fieldname": "image",
+        "originalname": "cat.webp",
+        "encoding": "7bit",
+        "mimetype": "image/webp",
+        "destination": "uploads/",
+        "filename": "b0d5f454b89d80f2ddd23761a8d2ffde",
+        "path": "uploads/b0d5f454b89d80f2ddd23761a8d2ffde",
+        "size": 592494
+    }
+    */
+
+    const [rows, [updatedGroup]] = await Group.update(
+      { imagePath: filename },
+      {
+        where: { id: groupId },
+        returning: true,
+      }
+    );
+
+    if(rows !== 1) {
+      return next(createHttpError(404, 'Group not found'));
+    }
+
+    res.send({ data: updatedGroup });
   } catch (error) {
     next(error);
   }
--- routers/groupRouter.js
+++ routers/groupRouter.js
@@ -1,6 +1,24 @@
 const groupRouter = require('express').Router();
+const path = require('path');
+const multer = require('multer');
 const GroupController = require('../controllers/groupController');
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, '../public/images'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 groupRouter.post('/', GroupController.createGroup);
+groupRouter.post(
+  '/:groupId/images',
+  upload.single('image'),
+  GroupController.addImageToGroup
+);
 
 module.exports = groupRouter;
