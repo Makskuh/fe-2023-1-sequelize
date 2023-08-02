6ba605a491befc8243555f01b0ce334deb0777c9
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-02 20:58:31 +0300

    Add CORS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
 const router = require('./routers');
 const ErrorHandlers = require('./middlewares/errors');
 const CONSTANTS = require('./constants');
@@ -7,6 +7,8 @@ const CONSTANTS = require('./constants');
 const app = express();
 
 const bodyParser = express.json();
+
+app.use(cors());
 app.use(bodyParser); // монтує міддлвери на будь-які методи (get, post ...)
 
 app.use(express.static(CONSTANTS.FILES_PATH));
