5af7a42414d1eed2356a8cd227ec5242508e4b5b
Max Kuh <maxes2805582@gmail.com>
2023-08-03 21:25:00 +0300

    add solution get users

--- seeders/20230724150033-generateUsers.js
+++ seeders/20230724150033-generateUsers.js
@@ -1,37 +1,37 @@
-'use strict';
+// 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('users', [
-      {
-        first_name: 'Admin',
-        last_name: 'Adminovich',
-        email: 'admin@mail.com',
-        password: 'adminAdmin12345',
-        is_male: true,
-        birthday: new Date(1978, 5, 13),
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        first_name: 'Moder',
-        last_name: 'Moderenko',
-        email: 'moder@mail.com',
-        password: 'notAdminAdmin12345',
-        is_male: true,
-        birthday: new Date(1994, 6, 15),
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.bulkInsert('users', [
+//       {
+//         first_name: 'Admin',
+//         last_name: 'Adminovich',
+//         email: 'admin@mail.com',
+//         password: 'adminAdmin12345',
+//         is_male: true,
+//         birthday: new Date(1978, 5, 13),
+//         created_at: new Date(),
+//         updated_at: new Date(),
+//       },
+//       {
+//         first_name: 'Moder',
+//         last_name: 'Moderenko',
+//         email: 'moder@mail.com',
+//         password: 'notAdminAdmin12345',
+//         is_male: true,
+//         birthday: new Date(1994, 6, 15),
+//         created_at: new Date(),
+//         updated_at: new Date(),
+//       },
+//     ]);
+//   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('users', {
-      email: {
-        [Sequelize.Op.or]: ['admin@mail.com', 'moder@mail.com'],
-      },
-    });
-  },
-};
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.bulkDelete('users', {
+//       email: {
+//         [Sequelize.Op.or]: ['admin@mail.com', 'moder@mail.com'],
+//       },
+//     });
+//   },
+// };
